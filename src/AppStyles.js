e9ab39c8f984e6feb73f315353feca755de8542b
Samuel Jo <jo.sunghak@gmail.com>
2021-08-18 11:29:46 -0400

    removed unused css

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -2,10 +2,6 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   container:{
-    // backgroundColor: "#7ee8fa",
-    // backgroundImage: "linear-gradient(315deg, #7ee8fa 0%, #80ff72 74%)",
-    background: "#2c3e50", /* fallback for old browsers */
-    background: "-webkit-linear-gradient(to right, #2c3e50, #4ca1af)", /* Chrome 10-25, Safari 5.1-6 */
     background: "linear-gradient(to right, #2c3e50, #4ca1af)", /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
     height: '100vh',
     overflowX: 'hidden',
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -21,20 +21,13 @@ export default makeStyles((theme) => ({
   textField:{
     "& input":{
       color:'#ffffff',
-      // background: "#FFEFBA",  /* fallback for old browsers */
-      // background: "-webkit-linear-gradient(to right, #FFFFFF, #FFEFBA)",  /* Chrome 10-25, Safari 5.1-6 */
-      // background: "linear-gradient(to right, #FFFFFF, #FFEFBA)", /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
       borderRadius: '6px',
-      background: "#606c88",  /* fallback for old browsers */
-      background: "--webkit-linear-gradient(to right, #606c88, #3f4c6b);",  /* Chrome 10-25, Safari 5.1-6 */
       background: "linear-gradient(to right, #606c88, #3f4c6b)", /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
     },
   },
   button:{
     marginLeft:'.5rem',
-    background: "#a8c0ff",  /* fallback for old browsers */
-    background: "-webkit-linear-gradient(to right, #3f2b96, #a8c0ff)",  /* Chrome 10-25, Safari 5.1-6 */
     background: "linear-gradient(to right, #3f2b96, #a8c0ff)", /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
     color:'white'
   }
