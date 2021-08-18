aa5bf3cc4dbefe3bc9c24755dcca654f6851ac71
Samuel Jo <jo.sunghak@gmail.com>
2021-08-18 15:56:43 -0400

    feat/classes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import { auth, db } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -16,7 +16,7 @@ import useStyles from "./AppStyles";
 function App() {
   const classes = useStyles();
   const [user] = useAuthState(auth);
-  const [balances, setBalances] = useState([])
+  const [balances, setBalances] = useState([]);
 
   useEffect(() => {
     if (user) {
@@ -33,36 +33,52 @@ function App() {
           ).toLocaleDateString("en-US");
           setBalances((prevState) => [
             ...prevState,
-            { timestamp: convertedDate, balance: doc.data().balance, id: doc.data().id },
+            {
+              timestamp: convertedDate,
+              balance: doc.data().balance,
+              id: doc.data().id,
+            },
           ]);
         });
       });
-    }else{
-      setBalances([])
+    } else {
+      setBalances([]);
     }
   }, [user]);
+
+  const updateState = (newBalances) => {
+    setBalances(newBalances)
+  }
 
   return (
     <div className={classes.container}>
       <Header />
-      {user ? (
-        <>
-        <Input />
-        <Grid container direction="row" justifyContent="center" alignItems="center" className={classes.grid}>
-          <Grid item xs={12} sm={12} md={12} lg={12}>
-            <UnitsChart mostRecentBalance={balances[0]}/>
-          </Grid>
-          <Grid item xs={12} sm={12} md={12} lg={12}>
-            <BalancesChart balances={balances}/>
-          </Grid>
-        </Grid>
-        </>
-      ):(
-        <div className={classes.login}>
-          <Auth className={classes.loginBtn}/>
-        </div>
-      )}
-      <Footer/>
+      <>
+        {user ? (
+          <>
+            <Input updateState={updateState}/>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              className={classes.grid}
+            >
+              <Grid item xs={12} sm={12} md={12} lg={12}>
+                <UnitsChart mostRecentBalance={balances[0]} />
+              </Grid>
+              <Grid item xs={12} sm={12} md={12} lg={12}>
+                <BalancesChart balances={balances} />
+              </Grid>
+            </Grid>
+          </>
+        ) : (
+          <div className={classes.login}>
+            <Auth className={classes.loginBtn} />
+          </div>
+        )}
+      </>
+      <Footer className={classes.footer} />
     </div>
   );
 }
